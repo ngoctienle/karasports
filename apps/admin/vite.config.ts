6dae49a820be344e7fc1a3ec38af8008b196831e
ngoctienle <ngoctienle.2399@gmail.com>
2023-12-17 22:07:19 +0700

    feat(*): init project with nx

---
+++ apps/admin/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/dashboard',
+  plugins: [react()],
+  build: {
+    outDir: '../../dist/apps/admin'
+  },
+  server: {
+    port: 3000
+  }
+})
---
+++ packages/generate/src/generators/reactlib/generator.ts
@@ -0,0 +1,27 @@
+import { formatFiles, generateFiles, names, Tree, readJson } from '@nx/devkit'
+import * as path from 'path'
+import { ReactlibGeneratorSchema } from './schema'
+
+export async function reactlibGenerator(
+  tree: Tree,
+  options: ReactlibGeneratorSchema
+) {
+  const scopeName = readJson(tree, 'package.json').name
+
+  const resolvedOptions = {
+    ...options,
+    name: names(options.name).fileName,
+    scope: scopeName
+  }
+
+  const projectRoot = `packages/${resolvedOptions.name}`
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    projectRoot,
+    resolvedOptions
+  )
+  await formatFiles(tree)
+}
+
+export default reactlibGenerator
