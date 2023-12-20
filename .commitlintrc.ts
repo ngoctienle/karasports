f6a1eb7572eae06bb8f8e3bc44daef6ea33e4f96
ngoctienle <ngoctienle.2399@gmail.com>
2023-12-20 20:55:15 +0700

    perf: setup eslint and husky for coding convention

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,7 +1,13 @@
 import type { UserConfig } from '@commitlint/types'
 
 const commitlintConfig: UserConfig = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [1, 'always', 100]
+  },
+  ignores: [
+    (commit) => commit.includes('Optimised images with calibre/image-actions')
+  ]
 }
 
 module.exports = commitlintConfig
